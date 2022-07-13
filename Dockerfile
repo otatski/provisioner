bb11228d4abcd9bdc6de26c1190cae93d189f801
Mat <mat@pop-os.localdomain>
2022-07-12 21:58:42 -0400

    Add first implementation of provisioner

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM ubuntu:22.04
+RUN apt update
+# sudo and firefox are needed to test CAC support.
+RUN apt install -y sudo wget firefox
+RUN useradd -ms /bin/bash docker && echo "docker:docker" | chpasswd && adduser docker sudo
+RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+USER docker
+WORKDIR /home/docker
+RUN sh -c "$(wget https://raw.githubusercontent.com/otatski/provisioner/main/provision.sh -O -)"
---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script is intended to stage the host for running ansible,
+# which will finish the provisioning process.
+
+main() {
+    apt update
+    DEBIAN_FRONTEND=noninteractive sudo apt install -y software-properties-common
+    apt-add-repository -y --update ppa:ansible/ansible
+    DEBIAN_FRONTEND=noninteractive sudo apt install -y ansible git
+    ansible-pull -U https://github.com/otatski/provisioner
+}
+
+main
